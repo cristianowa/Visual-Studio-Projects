5327073b906b10de8506610bc8efe99840d62507
Cristiano Araujo <cristianowerneraraujo@gmail.com>
2012-02-19 11:44:19 -0200

    Nokia E63 Contacts Converter. Output changed from CSV to TXT.

--- Nokia E63 Contacts Converter/Nokia E63 Contacts Converter/Program.cs
+++ Nokia E63 Contacts Converter/Nokia E63 Contacts Converter/Program.cs
@@ -8,11 +8,19 @@ namespace Nokia_E63_Contacts_Converter
 {
     class Program
     {
-        
+        static string spaces(int n)
+        {
+            string ret = "";
+            for (int i = 0; i < n; i++)
+                ret += " ";
+            return ret;
+        }
 
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines(@"C:\Users\Cristiano\Desktop\Nokia E63_2012-02-18.nbu");
+            File.Delete(@"C:\Users\Cristiano\Desktop\ContatosHugo.txt");
+            StreamWriter output = new StreamWriter(@"C:\Users\Cristiano\Desktop\ContatosHugo.txt");                
             for (int i = 0; i < 2406; i++)
             {
                 if(lines[i].Contains("BEGIN:VCARD"))
@@ -22,8 +30,12 @@ namespace Nokia_E63_Contacts_Converter
                     contactName = contactName.Substring(1);
                     contactName = contactName.Replace(";", "");
                     contactName = contactName.Replace(":", "");
+                    contactPhone =contactPhone.Substring(contactPhone.IndexOf(":")+1);
+                    output.WriteLine(contactName + spaces(30 - contactName.Length) + contactPhone);
                     System.Console.WriteLine(contactName);
                     System.Console.WriteLine(contactPhone);
+                    
+
                 }
             }
             while (true) { }
