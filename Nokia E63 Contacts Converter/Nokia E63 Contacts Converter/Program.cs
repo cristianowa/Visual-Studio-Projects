b826df041daae3c27aed3af1313a662c499740a4
Cristiano Araujo <cristianowerneraraujo@gmail.com>
2012-02-19 00:48:31 -0200

    Nokia E63 contact converter. ( from nokia suite file to CSV file )

---
+++ Nokia E63 Contacts Converter/Nokia E63 Contacts Converter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Nokia_E63_Contacts_Converter
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Cristiano\Desktop\Nokia E63_2012-02-18.nbu");
+            for (int i = 0; i < 2406; i++)
+            {
+                if(lines[i].Contains("BEGIN:VCARD"))
+                {//contact founded 
+                    string contactName = lines[i+3];
+                    string contactPhone = lines[i+4];
+                    contactName = contactName.Substring(1);
+                    contactName = contactName.Replace(";", "");
+                    contactName = contactName.Replace(":", "");
+                    System.Console.WriteLine(contactName);
+                    System.Console.WriteLine(contactPhone);
+                }
+            }
+            while (true) { }
+        }
+    }
+}
---
+++ Nokia E63 Contacts Converter/Nokia E63 Contacts Converter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nokia E63 Contacts Converter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nokia E63 Contacts Converter")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee182c6b-99b5-4d8b-b447-2b846d5b68b7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
